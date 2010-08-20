ea60f5591d5c0a3753ca93f80e1f83c973976ae4
Nicolas Sauzede <nicolas.sauzede@st.com>
2010-08-20 15:49:37 +0200

    implemented tga output support; support multi spheres

--- intersec.c
+++ intersec.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 #include <math.h>
 
-#define W 10
-#define H 10
+#define W 80
+#define H 60
 #define SMALL	0.001
 #define BIG		10.0
 
@@ -77,49 +77,121 @@ double *t)
 	return result;
 }
 
-int main()
-{
-	printf( "hello intersec\n");
-	
+typedef struct {
 	double cx, cy, cz, sr;
+	double r, g, b;
+} sphere_t;
+
+sphere_t spheres[] = {
+	{ .cx = 0.05, .cy = 0.1, .cz = 0.5, .sr = 0.2, .r = 1.0, .g = 0.0, .b = 0.0 },
+	{ .cx = 0.0, .cy = 0.0, .cz = 0.6, .sr = 0.1, .r = 0.0, .g = 1.0, .b = 0.0 },
+};
+int nsph = sizeof (spheres) / sizeof (spheres[0]);
+
+int main( int argc, char *argv[])
+{
 	double ex, ey, ez, vx, vy, vz;
 	
-	cx = 0.05; cy = 0.1; cz = 0.5; sr = 0.2;
 	ex = 0; ey = 0; ez = 1; vx = 0.05; vy = 0.1; vz = -1;
 	int w, h;
-	w = 8; h = 6;
+	w = W; h = H;
 	double winw, winh;
 	winw = 1.0; winh = 1.0;
-	
-	printf( "sphere: c(%f;%f;%f) r(%f)\n", cx, cy, cz, sr);
-	printf( "eye: e(%f;%f;%f) v(%f;%f;%f)\n", ex, ey, ez, vx, vy, vz);
+	int do_tga = 0;
+	int bpp = 24;
+	unsigned char tga_bdata;
+	unsigned short tga_sdata;
+
+	int arg = 1;
+	if (argc > arg)
+	{
+		sscanf( argv[arg++], "%d", &w);
+		if (argc > arg)
+		{
+			sscanf( argv[arg++], "%d", &h);
+			if (argc > arg)
+			{
+				sscanf( argv[arg++], "%d", &do_tga);
+			}
+		}
+	}
+	if (do_tga)
+	{
+#define TGA_BYTE(b) tga_bdata = b; write( 1, &tga_bdata, sizeof( tga_bdata))
+#define TGA_SHORT(s) tga_sdata = s; write( 1, &tga_sdata, sizeof( tga_sdata))
+		TGA_BYTE( 0);
+		TGA_BYTE( 0);
+		TGA_BYTE( 2);
+		TGA_BYTE( 0);
+		TGA_BYTE( 0);
+		TGA_BYTE( 0);
+		TGA_BYTE( 0);
+		TGA_BYTE( 0);
+		TGA_SHORT( 0);
+		TGA_SHORT( 0);
+		TGA_SHORT( w);
+		TGA_SHORT( h);
+		TGA_BYTE( bpp);
+		TGA_BYTE( 0x20);
+	}
+	else
+	{
+		printf( "w=%d h=%d\n", w, h);
+		printf( "eye: e(%f;%f;%f) v(%f;%f;%f)\n", ex, ey, ez, vx, vy, vz);
+	}
 	
 	int i, j;
 	for (j = 0; j < h; j++)
 	{
-	for (i = 0; i < w; i++)
-	{
-		int res;
-		double _vx, _vy, _vz;
-		_vx = vx - winw / 2 + winw * (double)i / (w - 1);
-		_vy = vy + winh / 2 - winh * (double)j / (h - 1);
-		_vz = vz;
-		double t = 0;
-		res = intersec_sphere( cx, cy, cz, sr, ex, ey, ez, _vx, _vy, _vz, &t);
-//		printf( "%c", res ? 's' : '.');
-		if (res)
+		for (i = 0; i < w; i++)
 		{
-#define COLS 9
-			int pix = (int)((BIG - (t - (double)SMALL)) * COLS / BIG);
-			if (pix < 0)
-				pix = -pix;
-			pix %= COLS;
-			printf( "%d", pix);
+			int s;
+			double tmin = BIG;
+			char pix = '.';
+			double rmin = 0.0, gmin = 0.0, bmin = 0.0;
+			for (s = 0; s < nsph; s++)
+			{
+				double cx, cy, cz, sr;
+				cx = spheres[s].cx; cy = spheres[s].cy; cz = spheres[s].cz; sr = spheres[s].sr;
+//				printf( "sphere: c(%f;%f;%f) r(%f)\n", cx, cy, cz, sr);
+				int res;
+				double _vx, _vy, _vz;
+				_vx = vx - winw / 2 + winw * (double)i / (w - 1);
+				_vy = vy + winh / 2 - winh * (double)j / (h - 1);
+				_vz = vz;
+				double t = 0;
+				res = intersec_sphere( cx, cy, cz, sr, ex, ey, ez, _vx, _vy, _vz, &t);
+//				printf( "%c", res ? 's' : '.');
+				if (res)
+				{
+					if (t < tmin)
+					{
+						tmin = t;
+						rmin = spheres[s].r; gmin = spheres[s].g; bmin = spheres[s].b;
+//						pix = '0' + s;
+						if (spheres[s].r > 0)
+							pix = 'r';
+						else if (spheres[s].g > 0)
+							pix = 'g';
+						else if (spheres[s].b > 0)
+							pix = 'b';
+						else
+							pix = 'k';
+					}
+				}
+			}
+			if (do_tga)
+			{
+				TGA_BYTE( (unsigned char)(255 * rmin));
+				TGA_BYTE( (unsigned char)(255 * gmin));
+				TGA_BYTE( (unsigned char)(255 * bmin));
+//				TGA_BYTE( 0);
+			}
+			else
+				printf( "%c", pix);
 		}
-		else
-			printf( ".");
-	}
-	printf( "\n");
+		if (!do_tga)
+			printf( "\n");
 	}
 	
 	return 0;
