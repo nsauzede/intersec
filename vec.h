04d67f35704fedb7a602ce52e4f2da5daa7f1488
Nicolas Sauzede <nicolas.sauzede@st.com>
2013-12-23 03:22:53 +0100

    convert bench to use vec

--- vec.h
+++ vec.h
@@ -11,6 +11,7 @@ double* mult3( v3 n, double nn);
 double* add3( v3 n, double nn);
 double dot3( v3 p, v3 n);
 double norm3( v3 n);
+void disp3( char *s, const v3 v);
 int intersec_plane( v3 p0, v3 p1, v3 p2, v3 l0, v3 l, double *pt);
 int solvetri( double a, double b, double c, double *t1, double *t2);
 int intersec_sphere( v3 cs, double sr, v3 e, v3 v, double *tmin, double *tmax);
